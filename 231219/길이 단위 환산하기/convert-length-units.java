ebb2fe00007ddea5967a7c6fb642b95541b31ad4
SMJminjeong <sdwp0920@gmail.com>
2023-12-19 07:13:27 +0900

    Title: 길이 단위 환산하기, Time: 161ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 231219/길이 단위 환산하기/convert-length-units.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        double answer = a * 30.48;
+
+        System.out.printf("%.1f", answer);
+
+    }
+}
