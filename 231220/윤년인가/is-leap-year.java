bc232471aa23325a8affacacc40248b13bfc5fed
SMJminjeong <sdwp0920@gmail.com>
2023-12-20 08:33:39 +0900

    Title: 윤년인가, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 231220/윤년인가/is-leap-year.java
+++ 231220/윤년인가/is-leap-year.java
@@ -5,7 +5,7 @@ public class Main {
 
         int year = scanner.nextInt();
         if(year % 4 == 0){
-            if((year % 400 == 0) && (year % 100 != 0) ){
+            if((year % 400 == 0) && (year % 100 == 0) ){
                 System.out.println("true"); 
             } else if (year % 100 == 0){
                 System.out.println("false");
