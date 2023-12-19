16940d2cbbe761988afc8461d5562c9a04beeeb5
SMJminjeong <sdwp0920@gmail.com>
2023-12-19 21:08:31 +0900

    Title: 숫자의 조건 여부 2, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231219/숫자의 조건 여부 2/number's-condition-2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        if(a == 5) System.out.println("A");
+        if(a % 2 == 0) System.out.println("B");
+    }
+}
