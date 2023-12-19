68d360b5054e6c9b746dc37b634b808c2c59e507
SMJminjeong <sdwp0920@gmail.com>
2023-12-19 10:15:25 +0900

    Title: 입력받아 출력 2, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231219/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter("-");
+
+        int front = scanner.nextInt();
+        int back = scanner.nextInt();
+
+        System.out.println(front+""+back);
+    }
+}
