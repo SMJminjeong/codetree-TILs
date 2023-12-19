1096505001d85927e750116cf72f4ef95e9dd97c
SMJminjeong <sdwp0920@gmail.com>
2023-12-19 20:20:12 +0900

    Title: 숫자의 조건 여부, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231219/숫자의 조건 여부/number's-condition.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        if(a>=113) System.out.println("1");
+        else System.out.println("0");
+    }
+}
