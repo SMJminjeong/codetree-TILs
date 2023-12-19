4c28b49e561f4b5e7495597022a320da55086583
SMJminjeong <sdwp0920@gmail.com>
2023-12-19 20:54:57 +0900

    Title: 알파벳에 따른 평가, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231219/알파벳에 따른 평가/evaluation-by-alphabet.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String alphabet = scanner.next();
+        if(alphabet.equals("S")) System.out.println("Superior");
+        else if(alphabet.equals("A")) System.out.println("Excellent");
+        else if(alphabet.equals("B")) System.out.println("Good");
+        else if(alphabet.equals("C")) System.out.println("Usually");
+        else if(alphabet.equals("D")) System.out.println("Effort");
+        else System.out.println("Failure");
+    }
+}
