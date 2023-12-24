5a569f0dd231eb284e8958254145e33345ca352f
SMJminjeong <sdwp0920@gmail.com>
2023-12-24 21:16:44 +0900

    Title: 100부터 입력받는 수까지의 합, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231224/100부터 입력받는 수까지의 합/sum-of-the-number-input-from-100.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int answer = 0;
+        for(int i = n; i<101; i++){
+            answer += i;
+        }
+        System.out.println(answer);
+    }
+}
