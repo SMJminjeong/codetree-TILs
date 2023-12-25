2c6a4497e1d6a071caca25b2199e2b5d02ee23f6
SMJminjeong <sdwp0920@gmail.com>
2023-12-25 14:13:36 +0900

    Title: 총합 구하기, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231225/총합 구하기/find-the-total.java
@@ -0,0 +1,18 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int a = Integer.parseInt(input[0]);
+        int b = Integer.parseInt(input[1]);
+        int answer = 0;
+
+        for(int i = a; i<=b ; i++){
+            if(i % 6 == 0 && i % 8 != 0){
+                answer += i;
+            }
+        }
+        System.out.print(answer);
+
+    }
+}
