c71c51044a95bb18a48e1d16f35482a3005c9289
SMJminjeong <sdwp0920@gmail.com>
2023-12-26 14:47:30 +0900

    Title: ab 사이에 없는 c, Time: 97ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231226/ab 사이에 없는 c/c-between-a-and-b-2.java
@@ -0,0 +1,24 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+
+        int a = Integer.parseInt(input[0]);
+        int b = Integer.parseInt(input[1]);
+        int c = Integer.parseInt(input[2]);
+        boolean isExist = false;
+
+        for(int i = a; i<=b; i++){
+            if(i % c == 0){
+                isExist = true;
+            }
+        }
+
+        if(isExist){
+            System.out.print("NO");
+        }else{
+            System.out.print("YES");
+        }
+    }
+}
