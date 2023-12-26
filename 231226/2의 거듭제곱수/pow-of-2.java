e165c67faf36e094edaf00b2594ecc2d3cee1f8a
SMJminjeong <sdwp0920@gmail.com>
2023-12-26 13:20:43 +0900

    Title: 2의 거듭제곱수, Time: 97ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 231226/2의 거듭제곱수/pow-of-2.java
+++ 231226/2의 거듭제곱수/pow-of-2.java
@@ -3,7 +3,14 @@ public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
-        int answer = (int)Math.pow(num, 0.5);
-        System.out.print(answer);
+        int x = 1;
+        while(true){
+            int answer = 2 * x;
+            if(answer == num){
+                System.out.print(x);
+                break;
+            }
+            x++;
+        }
     }
 }
