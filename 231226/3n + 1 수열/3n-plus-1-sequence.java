2a78d2cd10a1dfbe71f5249c3b05a97b3eb76b5d
SMJminjeong <sdwp0920@gmail.com>
2023-12-26 10:53:00 +0900

    Title: 3n + 1 수열, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231226/3n + 1 수열/3n-plus-1-sequence.java
@@ -0,0 +1,25 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        int count = 0;
+        while(true){
+            if(num % 2 == 0){
+                count++;
+                num /= 2;
+                if(num == 1){
+                    System.out.print(count);
+                    break;
+                }
+            }else{
+                count++;
+                num = num*3 +1;
+                if(num == 1){
+                    System.out.print(count);
+                    break;
+                }
+            }
+        }
+    }
+}
